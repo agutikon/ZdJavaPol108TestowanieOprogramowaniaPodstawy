129924547fcdb4d01508286a1752d2a0fca68c4b
Pawel Dudko <paweldudko@outlook.com>
2022-03-13 13:20:47 +0100

    unit_task2

---
+++ src/test/java/pl/sdacademy/unit/test/basic/exercises/unit/task2/StringUtilTest.java
@@ -0,0 +1,47 @@
+package pl.sdacademy.unit.test.basic.exercises.unit.task2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilTest {
+
+    /*
+    true:
+    ""
+    null
+    " "
+    new String()
+    "\n"
+
+    false:
+    "SDA"
+    "."
+    " SDA"
+    " SDA "
+    "SDA "
+    "null"
+     */
+
+    @Test
+    void shouldReturnTrueIfStringIsEmpty() {
+        //given
+        String input = "";
+        //when
+        boolean result = StringUtil.isBlank(input);
+        //then
+        assertTrue(result);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void shouldReturnFalseIfStringContainsText() {
+        //when
+        boolean result = StringUtil.isBlank("SDA");
+        //then
+        assertFalse(result);
+        assertThat(result).isFalse();
+    }
+
+}
